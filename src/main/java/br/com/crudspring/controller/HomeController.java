4f19aae0471684284361aa594395f112588ca902
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-06-29 21:27:04 -0300

    alterações

--- src/main/java/br/com/crudspring/controller/HomeController.java
+++ src/main/java/br/com/crudspring/controller/HomeController.java
@@ -1,14 +1,11 @@
 package br.com.crudspring.controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.crudspring.model.Usuario;
@@ -29,7 +26,7 @@ public class HomeController {
 	@PostMapping("/cadastrar")
 	public String cadastrar(Usuario usuario, RedirectAttributes attr) {
 		service.salvar(usuario);
-		attr.addFlashAttribute("success", "Usuario cadastrado com sucesso!");
+		attr.addFlashAttribute("success", "Usuário{a} cadastrado{a} com sucesso!");
 		return "redirect:/";
 	}
 	
@@ -42,13 +39,14 @@ public class HomeController {
 	@PostMapping("/editar")
 	public String editar(Usuario usuario, RedirectAttributes attr) {
 		service.salvar(usuario);
-		attr.addFlashAttribute("success", "Usuario alterado com sucesso!");
+		attr.addFlashAttribute("success", "Usuário{a} alterado{a} com sucesso!");
 		return "redirect:/";
 	}
 	
 	@GetMapping("/deletar/{id}")
-	public String excluir(@PathVariable("id") Long id) {
+	public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
 		service.excluirPorId(id);
+		attr.addFlashAttribute("success", "Usuário{a} excluído{a} com sucesso!");
 		return "redirect:/";
 		
 	}
